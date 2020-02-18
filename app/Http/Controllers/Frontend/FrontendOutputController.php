a943888ed583be0bb80d508090af8a962f29adda
Denis Bashkirov <era-digital@yandex.ru>
2020-02-18 23:24:46 +0300

    Create FrontendOutputController.php

---
+++ app/Http/Controllers/Frontend/FrontendOutputController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Events\LeadFormSubmitted;
+use App\Feedback;
+use App\ServiceType;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Route;
+use Cookie;
+
+class FrontendOutputController extends FrontendBaseController
+{
+
+    public function home(Request $request) {
+
+
+    }
+
+}
