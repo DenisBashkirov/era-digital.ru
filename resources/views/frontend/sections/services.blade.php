032be066b9588324c941e2756586b634fe962349
Denis Bashkirov <era-digital@yandex.ru>
2020-02-19 00:18:11 +0300

    Update services.blade.php

--- resources/views/frontend/sections/services.blade.php
+++ resources/views/frontend/sections/services.blade.php
@@ -1,6 +1,11 @@
-<section class="services">
+<section class="services container">
 
     <div class="services__img"></div>
-    <div class="services__list"></div>
+    <div class="services__list">
+        <div class="services__item">
+            <p class="services__item-title">Веб-сайты</p>
+            <p class="services__item-text">Разработка и обслуживание</p>
+        </div>
+    </div>
 
 </section>
