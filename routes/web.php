34ba99784f0ae7f1f9c3de0bbf1c044d0824be80
Denis Bashkirov <era-digital@yandex.ru>
2020-02-18 23:53:34 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+    ['namespace'=>'Frontend'],
+    function () {
+        Route::get('/', ['uses'=>'PagesOutputController@home', 'as'=>'home']);
+    }
+);
