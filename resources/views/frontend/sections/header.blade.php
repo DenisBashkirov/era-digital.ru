3bc7b2b92ef8e8f6fdd80b84c9592851822d1e99
Denis Bashkirov <era-digital@yandex.ru>
2020-02-19 12:34:54 +0300

    Update header.blade.php

--- resources/views/frontend/sections/header.blade.php
+++ resources/views/frontend/sections/header.blade.php
@@ -1,8 +1,8 @@
 <header class="header">
     <div class="header__container container">
         <div class="header__contacts">
-            <a href="mailto:info@era-digital.ru" class="header__email">info@era-digital.ru</a>
-            <a href="tel:79952210582" class="header__phone">8 (995) 221 05 82</a>
+            <a href="mailto:info@era-digital.ru" class="header__contacts-item header__email">info@era-digital.ru</a>
+            <a href="tel:79952210582" class="header__contacts-item header__phone">8 (995) 221 05 82</a>
         </div>
     </div>
 </header>
