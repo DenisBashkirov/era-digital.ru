4617ecc4fa8e96669329f8ddaeef7988bb43cfb4
Denis Bashkirov <era-digital@yandex.ru>
2020-02-20 02:38:52 +0300

    commit

--- resources/views/frontend/sections/services.blade.php
+++ resources/views/frontend/sections/services.blade.php
@@ -29,4 +29,64 @@
 
     </div>
 
+    <div class="service-profiles">
+        <div class="service-profiles__item">
+            <div class="service-profiles__item-img">
+                <img src="{{asset('img/services/laravel-logo.png')}}" alt="Laravel фреймворк логотип">
+            </div>
+            <div class="service-profiles__item-body">
+                <h3 class="services-profiles__item-header">РАЗРАБОТКА ВЕБ-САЙТОВ</h3>
+                <div class="services-profiles__item-text">
+                    <p>Составляем структуры и прототипы страниц</p>
+                    <p>Оформляем дизайн-макеты страниц</p>
+                    <p>Делаем адаптивную вёрстку</p>
+                    <p>Размещаем и запускаем проект на хостинге</p>
+                </div>
+            </div>
+        </div>
+        <div class="service-profiles__item">
+            <div class="service-profiles__item-img">
+                <img src="{{asset('img/services/bitrix24-logo.png')}}" alt="Bitrix24 логотип">
+            </div>
+            <div class="service-profiles__item-body">
+                <h3 class="services-profiles__item-header">ВНЕДРЕНИЕ CRM ПОМОЖЕТ</h3>
+                <div class="services-profiles__item-text">
+                    <p>Вести полноценную стандартизированную базу контактов</p>
+                    <p>Сделать всю работу менеджеров прозрачной и доступной для контроля в любой момент времени</p>
+                    <p>Получать подробнейшую аналитику – оценивать качество работы сотрудников, выявлять эффективные каналы привлечения клиентов и многое другое</p>
+                    <p>Разрабатывать стратегию дальнейшего развития бизнеса и повышения качества работы</p>
+                </div>
+            </div>
+        </div>
+        <div class="service-profiles__item">
+            <div class="service-profiles__item-img">
+                <img src="{{asset('img/services/google-ads-logo.png')}}" alt="Google Ads логотип">
+            </div>
+            <div class="service-profiles__item-body">
+                <h3 class="services-profiles__item-header">РЕКЛАМА В ИНТЕРНЕТЕ</h3>
+                <div class="services-profiles__item-text">
+                    <p>Запускаем рекламу в поисковых системах Яндекс и Google</p>
+                    <p>Запускаем рекламу в сетях РСЯ (Яндекс) и КМС (Google)</p>
+                    <p>Запускаем таргетинговую рекламу в социальных сетях</p>
+                    <p>Создаём фото и видео контент</p>
+                    <p>Формируем отчёты по финансовой эффективности кампаний</p>
+                </div>
+            </div>
+        </div>
+        <div class="service-profiles__item">
+            <div class="service-profiles__item-img">
+                <img src="{{asset('img/services/google-logo.png')}}" alt="Google логотип">
+            </div>
+            <div class="service-profiles__item-body">
+                <h3 class="services-profiles__item-header">SEO ПРОДВИЖЕНИЕ</h3>
+                <div class="services-profiles__item-text">
+                    <p>Анализируем работу сайта: находим технические и контентные ошибки</p>
+                    <p>Подбираем целевые поисковые запросы - формируем семантическое ядро</p>
+                    <p>Отслеживаем результативность страниц по ключевым запросам</p>
+                    <p>Формируем отчёты по финансовой эффективности продвижения</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </section>
