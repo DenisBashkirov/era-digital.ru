57c25fa7dedb620499947c3892b1d9370cf5cfeb
Denis Bashkirov <era-digital@yandex.ru>
2020-02-19 12:31:17 +0300

    Create header.blade.php

---
+++ resources/views/frontend/sections/header.blade.php
@@ -0,0 +1,6 @@
+<header class="header">
+    <div class="header__contacts">
+        <a href="mailto:info@era-digital.ru" class="header__email">info@era-digital.ru</a>
+        <a href="tel:79952210582" class="header__phone">8 (995) 221 05 82</a>
+    </div>
+</header>
