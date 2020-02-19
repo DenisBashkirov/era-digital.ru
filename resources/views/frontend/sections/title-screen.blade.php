737cb323f84551513bdccd309a855be5805b84fa
Denis Bashkirov <era-digital@yandex.ru>
2020-02-19 13:21:09 +0300

    Update title-screen.blade.php

--- resources/views/frontend/sections/title-screen.blade.php
+++ resources/views/frontend/sections/title-screen.blade.php
@@ -4,7 +4,7 @@
 
     <div class="title-screen__body">
         <div class="title-screen__logo">
-            <img src="{{asset('img/logos/titles-screen.png')}}" alt="Era Digital логотип">
+            <img src="{{asset('img/logos/title-screen.png')}}" alt="Era Digital логотип">
         </div>
         <h1 class="h1">Диджитал маркетинг</h1>
     </div>
