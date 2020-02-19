414d21616578092594f26b78d9193938cbfc1c00
Denis Bashkirov <era-digital@yandex.ru>
2020-02-19 12:28:38 +0300

    sass update

--- resources/views/frontend/sections/services.blade.php
+++ resources/views/frontend/sections/services.blade.php
@@ -10,6 +10,21 @@
                 <div class="services__item-line"></div>
                 <p class="services__item-text">Разработка и обслуживание</p>
             </div>
+            <div class="services__item">
+                <p class="services__item-title">Внедрение CRM</p>
+                <div class="services__item-line"></div>
+                <p class="services__item-text">Помогает бизнесу работать</p>
+            </div>
+            <div class="services__item">
+                <p class="services__item-title">Реклама</p>
+                <div class="services__item-line"></div>
+                <p class="services__item-text">Контекстная и таргетинговая</p>
+            </div>
+            <div class="services__item">
+                <p class="services__item-title">SEO</p>
+                <div class="services__item-line"></div>
+                <p class="services__item-text">Продвижение в поисковых системах</p>
+            </div>
         </div>
 
     </div>
